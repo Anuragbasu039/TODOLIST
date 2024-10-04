b035f8b1abc019d6c63d5893efd3e98be7260ec4
anuragbasu039 <anuragbasu039@gmail.com>
2024-10-04 22:03:51 +0530

    signup tested successfully

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 const Signup = () => {
     const [formData, setFormData] = useState({
+        username: '', 
         email: '',
         password: '',
-        confirmPassword: ''
     });
 
     const handleChange = (e) => {
@@ -14,15 +14,12 @@ const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (formData.password !== formData.confirmPassword) {
-            alert('Passwords do not match');
-            return;
-        }
 
         try {
-            const res = await axios.post('http://localhost:5000/signup', {
+            const res = await axios.post('http://localhost:8080/api/signup', {
+                username: formData.username, 
                 email: formData.email,
-                password: formData.password
+                password: formData.password,
             });
             alert(res.data);
         } catch (error) {
@@ -34,6 +31,19 @@ const Signup = () => {
         <>
             <form className="mb-4 md:flex md:flex-wrap md:justify-between" onSubmit={handleSubmit}>
                 <h1 className="block w-full text-center text-2xl font-bold mb-6">Signup</h1>
+
+                <div className="flex flex-col mb-6 md:w-full">
+                    <label className="mb-2 font-bold text-lg text-grey-darkest" htmlFor="username">Username</label>
+                    <input
+                        className="border py-2 px-3 text-grey-darkest"
+                        type="text"
+                        name="username"
+                        id="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                    />
+                </div>
+
                 <div className="flex flex-col mb-4 md:w-full">
                     <label className="mb-2 font-bold text-lg text-grey-darkest" htmlFor="email">Email</label>
                     <input
@@ -45,6 +55,7 @@ const Signup = () => {
                         onChange={handleChange}
                     />
                 </div>
+
                 <div className="flex flex-col mb-6 md:w-full">
                     <label className="mb-2 font-bold text-lg text-grey-darkest" htmlFor="password">Password</label>
                     <input
@@ -56,17 +67,7 @@ const Signup = () => {
                         onChange={handleChange}
                     />
                 </div>
-                <div className="flex flex-col mb-6 md:w-full">
-                    <label className="mb-2 font-bold text-lg text-grey-darkest" htmlFor="confirmPassword">Confirm Password</label>
-                    <input
-                        className="border py-2 px-3 text-grey-darkest"
-                        type="password"
-                        name="confirmPassword"
-                        id="confirmPassword"
-                        value={formData.confirmPassword}
-                        onChange={handleChange}
-                    />
-                </div>
+
                 <div className="flex items-center justify-between gap-60">
                     <button
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
